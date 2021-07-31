0a9bb4c819fd56b49d622bd08b62f428732993f1
Brandon Maurice <bmaurice100@gmail.com>
2021-07-30 22:28:01 -0700

    Python Tester files

---
+++ Hello-World-Testing/Readme.md
@@ -0,0 +1,2 @@
+This project folder is for testing Python3 features and applications
+before utilizing its structures in projects.
---
+++ Hello-World-Testing/helloTester.py
@@ -0,0 +1,33 @@
+import sys
+import getopt
+
+# Print Hello world
+print("Hello world\n")
+hello = "Hello"
+world = "World"
+print(hello, world, world, hello, "\n")
+hWorld = ["hello", "world"]
+print(hWorld)
+print(hWorld[0])
+
+# Return types
+print(type("def"))
+print(type(b"def"))
+print(type("D"))
+print(type(3))
+print(type(3.3))
+
+# Return range() of number
+for x in range(1, 5):
+    print(x)
+
+# Error Handling
+try:
+    trying_to_catch_error
+
+except NameError as err:
+    print(err, "error caused")
+
+# Arithmetic
+print(4 + 3)
+print(29 / 3)
