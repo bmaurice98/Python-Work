6462b290d54870a9632c526abb30f5eaad3d060a
Brandon Maurice <bmaurice100@gmail.com>
2021-08-02 14:09:57 -0700

    Finished cartoonifying the image now plots each image throughout the process
    Building save button

--- Machine-Learning/Cartoonify/cartoonify.py
+++ Machine-Learning/Cartoonify/cartoonify.py
@@ -42,13 +42,42 @@ def cartoonify(ImagePath):
     )
     ReSized4 = cv2.resize(getEdge, (960, 540))
     # plt.imshow(ReSized4, cmap="gray")
-    
+
     # applying bilateral filter to remove image noise and keep edges sharp
     colorImage = cv2.bilateralFilter(originalImage, 9, 300, 300)
     ReSized5 = cv2.resize(colorImage, (960, 540))
     # plt.imshow(ReSized4, cmap="gray")
 
-    
+    # masking edged image with the beautified image
+    cartoonImage = cv2.bitwise_and(colorImage, colorImage, mask=getEdge)
+    ReSized6 = cv2.resize(cartoonImage, (960, 540))
+    # plt.imshow(ReSized6, cmap="gray")
+    # plt.show()
+
+    # plotting the transition
+    images = [ReSized1, ReSized2, ReSized3, ReSized4, ReSized5, ReSized6]
+    fig, axes = plt.subplots(
+        3,
+        2,
+        figsize=(8, 8),
+        subplot_kw={"xticks": [], "yticks": []},
+        gridspec_kw=dict(hspace=0.1, wspace=0.1),
+    )
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i], cmap="gray")
+    plt.show()
+
+
+def saveButton(Resized6, ImagePath):
+    # saving image with imwrite()
+    newName = "cartoon_Image"
+    path1 = os.path.dirname(ImagePath)
+    extension = os.path.splitext(ImagePath)[1]
+    path = os.path.join(path1, newName + extension)
+    cv2.imwrite(path, cv2.cvtColor(Resized6, cv2.COLOR_RGB2BGR))
+    I = "Image saved by name " + newName + " at " + path
+    tk.Messagebox.showinfo(title=None, Message=I)
+
 
 if __name__ == "__main__":
     upload()
